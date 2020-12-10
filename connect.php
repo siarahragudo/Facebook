01a302b82376fcee9b33770988df3ad2ea748d75
siarahragudo <75131052+siarahragudo@users.noreply.github.com>
2020-12-10 05:20:26 -0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+    //initialization
+    $servername = "localhost";
+	$username = "root";
+	$password = "";
+	$databasename  = "dbfacebook";
+
+
+	$connect = mysqli_connect($servername,$username,$password,$databasename);
+?>
